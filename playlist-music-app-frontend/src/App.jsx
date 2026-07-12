266b5f27a846fa36a2a4c73cbce524ff627b48c6
MathewAyala <mathewayala05@gmail.com>
2026-07-11 22:51:54 -0400

    created a routes file, and updated app.jsx

--- playlist-music-app-frontend/src/App.jsx
+++ playlist-music-app-frontend/src/App.jsx
@@ -1,15 +1,31 @@
-// import {BrowserRouter , Routes , Route } from "react-router";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-function App() {
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import './index.css'
+import './App.css'
+import AppRoutes from "./routes.jsx";
 
-  return (
 
-    <div>
-      <Navbar/>
-      <Home/>
-    </div>
-  )
-}
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+    
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </StrictMode>,
+)
+// import Home from "./pages/Home";
+// import Navbar from "./components/Navbar";
+// function App() {
 
-export default App
+//   return (
+
+
+//     <div>
+//       <Navbar/>
+//       <Home/>
+//     </div>
+//   )
+// }
+
+// export default App
